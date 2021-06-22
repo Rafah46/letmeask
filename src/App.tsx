fc2434c5e5b5c78b7927fdfa6c2f6de1e55a852e
Rafael Henrique <rhenrique.dev@outlook.com>
2021-06-21 22:17:32 -0300

    Stage 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "./components/Button";
+import { CounterButton } from "./components/CounterButton";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Button text="Botão 1" />
+
+			<CounterButton />
+			<CounterButton />
+			<CounterButton />
+			<CounterButton />
+		</div>
+	);
 }
 
 export default App;
