447daa6f37438e061ca89f13d6e2ff837e84fb7a
Rafael Henrique <rhenrique.dev@outlook.com>
2021-06-23 20:59:43 -0300

    Stage 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Button } from "./components/Button";
-import { CounterButton } from "./components/CounterButton";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-	return (
-		<div>
-			<Button text="Botão 1" />
 
-			<CounterButton />
-			<CounterButton />
-			<CounterButton />
-			<CounterButton />
-		</div>
+	return (
+		<BrowserRouter>
+			<AuthContextProvider>
+				<Route path="/" exact component={Home} />
+				<Route path="/rooms/new" exact component={NewRoom} />
+			</AuthContextProvider>
+		</BrowserRouter>
 	);
 }
 
